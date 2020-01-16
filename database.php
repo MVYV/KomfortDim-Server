c32ae5ec2c337053b543ff0ac43db86bb6548b82
YuriyValkiv <yurivalkiv@gmail.com>
2020-01-16 22:24:12 +0100

    db changes 1.4.2

--- database.php
+++ database.php
@@ -6,6 +6,17 @@ $username = $url["user"];
 $password = $url["pass"];
 $database = substr($url["path"], 1);
 
+'your_heroku_mysql_connection' => array(
+    'driver' => 'mysql',
+    'host' => $host,
+    'database' => $database,
+    'username' => $username,
+    'password' => $password,
+    'charset' => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix' => '',
+),
+
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: PUT, GET, POST, DELETE");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
@@ -17,7 +28,7 @@ define('DB_NAME', 'komfort_dim');
 
 function connect()
 {
-    $connect = mysqli_connect($host ,$username ,$password ,$database);
+    $connect = mysqli_connect('eu-cdbr-west-02.cleardb.net' ,'b772cc46f45e5a' ,'b98d3d89' ,'komfort_dim');
 
     if (mysqli_connect_errno($connect)) {
         die("Failed to connect:" . mysqli_connect_error());
