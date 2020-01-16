101ff33466654d2562a308d9694e23c0c8c04587
YuriyValkiv <yurivalkiv@gmail.com>
2020-01-16 22:26:41 +0100

    db changes 1.4.3

--- database.php
+++ database.php
@@ -1,34 +1,16 @@
 <?php
-$url = parse_url(getenv("mysql://b772cc46f45e5a:b98d3d89@eu-cdbr-west-02.cleardb.net/DATABASE?reconnect=true"));
-
-$host = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$database = substr($url["path"], 1);
-
-'your_heroku_mysql_connection' => array(
-    'driver' => 'mysql',
-    'host' => $host,
-    'database' => $database,
-    'username' => $username,
-    'password' => $password,
-    'charset' => 'utf8',
-    'collation' => 'utf8_unicode_ci',
-    'prefix' => '',
-),
-
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: PUT, GET, POST, DELETE");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'eu-cdbr-west-02.cleardb.net');
 define('DB_USER', 'b772cc46f45e5a');
 define('DB_PASS', 'b98d3d89');
 define('DB_NAME', 'komfort_dim');
 
 function connect()
 {
-    $connect = mysqli_connect('eu-cdbr-west-02.cleardb.net' ,'b772cc46f45e5a' ,'b98d3d89' ,'komfort_dim');
+    $connect = mysqli_connect(DB_HOST ,DB_USER ,DB_PASS ,DB_NAME);
 
     if (mysqli_connect_errno($connect)) {
         die("Failed to connect:" . mysqli_connect_error());
