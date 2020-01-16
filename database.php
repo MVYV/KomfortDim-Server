a860efac6536f10b45c2e905f848d236d38f053d
YuriyValkiv <yurivalkiv@gmail.com>
2020-01-16 22:37:54 +0100

    db changes 1.4.5

--- database.php
+++ database.php
@@ -1,4 +1,10 @@
 <?php
+$url = parse_url(getenv("mysql://b772cc46f45e5a:b98d3d89@eu-cdbr-west-02.cleardb.net/DATABASE?reconnect=true"));
+$host = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$database = substr($url["path"], 1);
+
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: PUT, GET, POST, DELETE");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
@@ -6,11 +12,11 @@ header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Ac
 define('DB_HOST', 'eu-cdbr-west-02.cleardb.net');
 define('DB_USER', 'b772cc46f45e5a');
 define('DB_PASS', 'b98d3d89');
-define('DB_NAME', 'heroku_925a66408b21b09');
+define('DB_NAME', 'komfort_dim');
 
 function connect()
 {
-    $connect = mysqli_connect(DB_HOST ,DB_USER ,DB_PASS ,DB_NAME);
+    $connect = mysqli_connect(DB_HOST ,DB_USER ,DB_PASS ,@database);
 
     if (mysqli_connect_errno($connect)) {
         die("Failed to connect:" . mysqli_connect_error());
