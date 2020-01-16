1d1db4b04897a864bfa1c692a25a6b6ad9a97e38
YuriyValkiv <yurivalkiv@gmail.com>
2020-01-16 22:38:55 +0100

    db changes 1.4.5.1

--- database.php
+++ database.php
@@ -16,7 +16,7 @@ define('DB_NAME', 'komfort_dim');
 
 function connect()
 {
-    $connect = mysqli_connect(DB_HOST ,DB_USER ,DB_PASS ,@database);
+    $connect = mysqli_connect(DB_HOST ,DB_USER ,DB_PASS ,$database);
 
     if (mysqli_connect_errno($connect)) {
         die("Failed to connect:" . mysqli_connect_error());
